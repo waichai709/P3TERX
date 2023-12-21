26a1e6e59317a99dfbcdaa81689cecc91c18a4b8
devilwai123 <153901841+devilwai123@users.noreply.github.com>
2023-12-22 01:33:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git openwrt https://github.com/robimarko/openwrt' >>feeds.conf.default
+#echo 'src-git openwrt https://github.com/robimarko/openwrt' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small >>feeds.conf.default
