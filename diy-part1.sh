a917d509f1aec2c856bd973eb1d8ce30af9e8cbc
devilwai123 <153901841+devilwai123@users.noreply.github.com>
2023-12-22 01:33:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git openwrt https://github.com/robimarko/openwrt' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
